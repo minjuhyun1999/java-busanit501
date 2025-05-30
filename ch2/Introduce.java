1f5d97c9259c9ed9a66d3155ba6b5c1ca755752e
minjuhyun1999 <minjoohyun67@naver.com>
2025-05-30 16:19:45 +0900

    동해물과

--- ch2/Introduce.java
+++ ch2/Introduce.java
@@ -1,6 +1,7 @@
 package ch2;
 
-public class Hello1 {
+public class Introduce {
+
     // 메소드1
     public static int sum(int n, int m) {
         return n + m;
@@ -24,6 +25,18 @@ public class Hello1 {
     // 5
     public static int div_float(int n, int m) {
         return n / m;
+    }
+
+    public static String introduce(String name, String phone, String email) {
+        return "저는 " + name + " 입니다.\n" +
+                "제 전화번호 : " + phone + " 이고,\n" +
+                "이메일 : " + email + " 입니다.";
+    }
+
+    public static String introduce1(String food, String hobby, String game) {
+        String introduce_myself = "제가 좋아하는 음식은 {" + food + "} 입니다. 제 취미는 {" + hobby + "}이고, 좋아하는 게임은 {" + game
+                + "}입니다.";
+        return introduce_myself;
     }
 
     public static void main(String[] args) {
@@ -72,6 +85,38 @@ public class Hello1 {
         // 8)정적 메소드를 사용(호출), 사용 시, 정의한 메소드로 가서, 액션을 하고 돌아와서 재사용.
         // 9)클래스 명, 대문자로 시작하고, 패키지 명과, 메소드(함수)명 은 소문자로 시작하고
         // 10)이름 작성시, 사용 가능한 특수 문자는 $,_, 달러 기호와 언버바만 사용 가능.
+
+        int s; // 변수를 정의만 했고, 실제 할당은 안한 상태,
+        char a; // 데이터 타입 char, 한 문자만을 의미함.
+        s = 100;
+        a = 'd';
+        System.out.println("정수 표현 :" + s);
+        System.out.println("문자 표현 :" + a);
+
+        // 퀴즈 4
+        // 본인의 이름과, 전화번호, 이메일을 입력 받는 매개변수가 3개이고,
+        // 출력값은 문자열 타입인데 : 결과는
+        // 저는 {이름} 입니다.
+        // 제 전화번호 : {전화번호} 이고,
+        // 이메일 : {이메일} 입니다.
+        // 출력하는 예제 해보기.
+
+        // 예제 호출
+        // 위에서 정의한 함수를 사용하기
+        String result6 = introduce("민주현", "010-9453-2254", "minjoohyun67@naver.com");
+        System.out.println(result6);
+
+        // 퀴즈,
+        // 내가 좋아하는 음식, 취미, 게임 등 알려주는 기능
+        // 좋아하는 음식, 취미, 게임 매개변수가 3개 정의
+        // 출력
+        // 제가 좋아하는 음식은 {음식} 입니다.
+        // 제 취미는 : {취미}이고,
+        // 좋아하는 게임: {게임명} 입니다.
+        // 출력하는 예제 해보기
+        String introduce_info = introduce1("삼겹살", "게임", "리그오브레전드");
+        System.out.println(introduce_info);
+
     }
 
 }
---
+++ util/UserService.java
@@ -0,0 +1,88 @@
+package util;
+
+import java.util.Scanner;
+
+public class UserService {
+
+    public static void registerUser(Scanner scanner) {
+
+        System.out.println("이상용 홈피 회원가입");
+        System.out.println("이름 입력해주세요 > ");
+        String name = scanner.nextLine(); // 이름 입력 받기
+        System.out.println("이메일 입력해주세요 > ");
+        String email = scanner.nextLine(); // 이메일 입력 받기
+
+        // 기능 추가
+        // ============================================================================
+        String password;
+        // while 문에서, 패스워드 부분이 입력이 맞으면 다음 단계, 아니면 , 계속 확인하기.
+        while (true) {
+            System.out.println("패스워드 입력해주세요 > ");
+            password = scanner.nextLine(); // 패스워드 입력 받기
+
+            System.out.println("패스워드 확인 입력해주세요 > ");
+            String password2 = scanner.nextLine(); // 패스워드 입력 받기
+
+            // 패스워드가 비어있지 않은지 확인
+            if (password.isEmpty()) {
+                System.out.println("패스워드는 비워둘 수 없습니다. 다시 입력해주세요.");
+            } else if (password.equals(password2)) { // 패스워드와 확인 입력이 일치하는지 확인
+                System.out.println("패스워드가 일치합니다");
+                break; // 패스워드가 일치하면 루프 종료
+            } else {
+                System.out.println("패스워드가 일치하지 않습니다. 다시 입력해주세요.");
+            }
+        }
+        // 기능 추가
+        // ============================================================================
+        System.out.println("이름 : " + name);
+        System.out.println("이메일 : " + email);
+        System.out.println("패스워드 : " + password);
+        System.out.println("회원 가입 완료되었습니다.");
+
+    }
+
+    public static void loginUser(Scanner scanner) { //
+
+        System.out.println("이상용 홈피 로그인");
+        System.out.println("이메일 입력해주세요 > ");
+        String email = scanner.nextLine(); // 이메일 입력 받기
+        System.out.println("패스워드 입력해주세요 > ");
+        String password = scanner.nextLine(); // 패스워드 입력 받기
+
+        System.out.println("로그인 이메일 정보 : " + email);
+        System.out.println("로그인 패스워드 정보 : " + password);
+        System.out.println("로그인 완료되었습니다. 현재 임시로 단순 출력용입니다.");
+    }
+    // 퀴즈,
+    // 클래스명, UserService_doc.java 여기서 작업 후,
+    // 클래스명 , UserService.java로 복사해서 붙여넣기
+    // 적용 클래스 : MainClass.java 실행 해보기.
+    // 참고 내용은.
+    // String password;
+    // // while 문에서, 패스워드 부분이 입력이 맞으면 다음 단계, 아니면 , 계속 확인하기.
+    // while (true) {
+    // System.out.println("패스워드 입력해주세요 > ");
+    // password = scanner.nextLine(); // 패스워드 입력 받기
+
+    // System.out.println("패스워드 확인 입력해주세요 > ");
+    // String password2 = scanner.nextLine(); // 패스워드 입력 받기
+
+    // // 패스워드가 비어있지 않은지 확인
+    // if (password.isEmpty()) {
+    // System.out.println("패스워드는 비워둘 수 없습니다. 다시 입력해주세요.");
+    // } else if (password.equals(password2)) { // 패스워드와 확인 입력이 일치하는지 확인
+    // System.out.println("패스워드가 일치합니다");
+    // break; // 패스워드가 일치하면 루프 종료
+    // } else {
+    // System.out.println("패스워드가 일치하지 않습니다. 다시 입력해주세요.");
+    // }
+    // }
+
+    // 유효성 체크를 , 로그인에도 적용해보기.
+    // 로그인에서, 데이터 베이스가 없으니, email: admin@naver.com, 패스워드: 1234로 가정하고,
+    // 일치 하는 경우에만, "로그인 성공" 메시지 출력,
+    // 일치하지 않는 경우, "로그인 실패" 메시지 출력.
+    // 이메일 비워져 있으면, "이메일은 비워둘 수 없습니다." 메시지 출력,
+    // 패스워드 비워져 있으면, "패스워드는 비워둘 수 없습니다." 메시지 출력.
+}
